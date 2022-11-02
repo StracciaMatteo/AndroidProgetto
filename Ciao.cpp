f863d1b3d38298e65a8e40b911080730704fef51
Matteo Straccia <matteo.straccia98@gmail.com>
2022-11-02 15:40:53 +0100

    Aggiunta classe Ciao

---
+++ Ciao.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matteo Straccia on 02/11/2022.
+//
+
+#include "Ciao.h"
---
+++ Ciao.h
@@ -0,0 +1,16 @@
+//
+// Created by Matteo Straccia on 02/11/2022.
+//
+
+#ifndef ANDROIDPROGETTO_CIAO_H
+#define ANDROIDPROGETTO_CIAO_H
+
+
+
+class Ciao {
+
+};
+
+
+
+#endif //ANDROIDPROGETTO_CIAO_H
