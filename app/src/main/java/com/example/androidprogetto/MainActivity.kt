58746eb81883ad342e38c8e0a718f767f85b354c
Matteo Straccia <matteo.straccia98@gmail.com>
2022-11-14 12:00:48 +0100

    Menu in activity

--- app/src/main/java/com/example/androidprogetto/MainActivity.kt
+++ app/src/main/java/com/example/androidprogetto/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidprogetto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 
 
@@ -18,4 +19,27 @@ class MainActivity : AppCompatActivity() {
         getMenuInflater().inflate(R.menu.toolbarmenu, menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item:MenuItem): Boolean{
+        when (item.itemId) {
+            R.id.ordina -> {
+                println("Ordina")
+            }
+            R.id.prodotti -> {
+                println("Prodotti")
+            }
+            R.id.calendarioeventi -> {
+                println("Calendario Eventi")
+            }
+            R.id.offerte -> {
+                println("Offerte")
+            }
+            R.id.recensioni -> {
+                println("Recensioni")
+            }
+
+        }
+        return true
+    }
+
 }
