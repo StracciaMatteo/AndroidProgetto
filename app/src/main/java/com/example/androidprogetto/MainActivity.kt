22fd4bed3add5078298962229c4124899f8fb757
Matteo Straccia <matteo.straccia98@gmail.com>
2022-11-14 11:39:55 +0100

    Push per menu in activity

--- app/src/main/java/com/example/androidprogetto/MainActivity.kt
+++ app/src/main/java/com/example/androidprogetto/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.androidprogetto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
     }
 
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.toolbarmenu, menu)
+        return true
     }
 }
