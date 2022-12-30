6dbc19f5692cbb0af9367267ac79e61341c508a1
Alex Rinaldi <s1096206@studenti.univpm.it>
2022-12-30 18:35:14 +0100

    aggiunto bottone utente con popup menu su visualizza_offerte.kt e su scelta servizio.kt che ora ha le immagini cliccabili che rimandano a selezione prodotti.kt

--- app/src/main/java/com/example/androidprogetto/visualizza_offerte.kt
+++ app/src/main/java/com/example/androidprogetto/visualizza_offerte.kt
@@ -3,8 +3,11 @@ package com.example.androidprogetto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.PopupMenu
 import androidx.appcompat.widget.Toolbar
 
 class visualizza_offerte : AppCompatActivity() {
@@ -12,6 +15,51 @@ class visualizza_offerte : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visualizza_offerte)
         setSupportActionBar(findViewById<Toolbar>(R.id.toolbarmenu))
+
+        val imageUtente = findViewById<ImageView>(R.id.utente)
+        imageUtente.setOnClickListener{
+            val popupMenu = PopupMenu(this, it)
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId){
+                    R.id.accedi->{
+                        val intent = Intent(this, login::class.java)
+                        startActivity(intent)
+                        true
+                    }
+                    R.id.tuoipreferiti->{
+                        val intent = Intent(this, listapreferiti::class.java)
+                        startActivity(intent)
+                        true}
+                    R.id.profilo->{
+                        val intent = Intent(this, visualizzaprofilo::class.java)
+                        startActivity(intent)
+                        true}
+                    R.id.esci->{
+                        val intent = Intent(this, login::class.java)
+                        startActivity(intent)
+                        true}
+
+                    else -> false
+                }
+            }
+
+            popupMenu.inflate(R.menu.pmenu)
+            try {
+                val fieldMPopup= PopupMenu::class.java.getDeclaredField("mPopup")
+                fieldMPopup.isAccessible = true
+                val mPopup = fieldMPopup.get(popupMenu)
+                mPopup.javaClass
+                    .getDeclaredMethod("setForceShowIcon",Boolean::class.java)
+                    .invoke(mPopup,true)
+            }
+            catch (e: Exception){
+                Log.e("main", "Errore nel mostrare il menu", e)
+            }
+            finally {
+                popupMenu.show()
+            }
+
+        }
     }
 
 
