e11f407cb19de4cbdf6bf11632908d8941e21528
Matteo Straccia <matteo.straccia98@gmail.com>
2023-01-26 16:12:39 +0100

    Activity collegate schermate Recensioni+Nuova Recensione

--- app/src/main/java/com/example/androidprogetto/recensioni.kt
+++ app/src/main/java/com/example/androidprogetto/recensioni.kt
@@ -1,11 +1,25 @@
 package com.example.androidprogetto
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class recensioni : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recensioni)
+
+
+        /*FUNZIONE PER PASSARE AD UN'ALTRA ACTIVITY TRAMITE CLICK SUL BOTTONE*/
+        val floatAdd = findViewById<FloatingActionButton>(R.id.addRecensione)
+        floatAdd.setOnClickListener {
+            val intent = Intent(this, caricamentorecensioni::class.java)
+            startActivity(intent)
+        }
+
     }
 }
