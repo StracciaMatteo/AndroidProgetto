c7f743833f0fd52d7f54b30987dbd1dc64f5d1cd
Matteo Straccia <matteo.straccia98@gmail.com>
2023-01-31 15:55:34 +0100

    Registrazione+Personalizzazione

--- app/src/main/java/com/example/androidprogetto/registrazione.kt
+++ app/src/main/java/com/example/androidprogetto/registrazione.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
 
 class registrazione : AppCompatActivity() {
@@ -40,13 +41,67 @@ class registrazione : AppCompatActivity() {
                 // Successfully signed in
                 val user = FirebaseAuth.getInstance().currentUser
                 // ...
-            } else {
+            }
+            else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
                 // response.getError().getErrorCode() and handle the error.
                 // ...
             }
         }
+
+
+
+        val actionCodeSettings = ActionCodeSettings.newBuilder()
+            .setAndroidPackageName( /* yourPackageName= */
+                "...",  /* installIfNotAvailable= */
+                true,  /* minimumVersion= */
+                null)
+            .setHandleCodeInApp(true) // This must be set to true
+            .setUrl("https://androidprogetto.page.link/76UZ") // This URL needs to be whitelisted
+            .build()
+
+        val providers = listOf(
+            AuthUI.IdpConfig.EmailBuilder()
+                .enableEmailLinkSignIn()
+                .setActionCodeSettings(actionCodeSettings)
+                .build()
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+
+
+
+        if (AuthUI.canHandleIntent(intent)) {
+            val extras = intent.extras ?: return
+            val link = extras.getString("email_link_sign_in") //VERIFICARE SE BISOGNA PASSARGLI LA MAIL
+            if (link != null) {
+                val signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setEmailLink(link)
+                    .setAvailableProviders(providers)
+                    .build()
+                signInLauncher.launch(signInIntent)
+            }
+        }
+
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                //METTERE BOTTONE ESCI
+            }
+
+        /*
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setLogo(R.drawable.my_great_logo) // Set logo drawable
+            .setTheme(R.style.MySuperAppTheme) // Set theme
+            .build()
+        signInLauncher.launch(signInIntent)*/
 
     }
 }
