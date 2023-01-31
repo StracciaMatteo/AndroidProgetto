65cc2dc23b87563f8b4f04753f25151e44ccdbca
Matteo Straccia <matteo.straccia98@gmail.com>
2023-01-31 12:30:18 +0100

    Prova Auth

--- app/src/main/java/com/example/androidprogetto/login.kt
+++ app/src/main/java/com/example/androidprogetto/login.kt
@@ -1,11 +1,39 @@
 package com.example.androidprogetto
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.ktx.actionCodeSettings
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val actionCodeSettings = actionCodeSettings {
+            // URL you want to redirect back to. The domain (www.example.com) for this
+            // URL must be whitelisted in the Firebase Console.
+            url = "https://www.example.com/finishSignUp?cartId=1234"
+            // This must be true
+            handleCodeInApp = true
+            setIOSBundleId("com.example.ios")
+            setAndroidPackageName(
+                "com.example.android",
+                true, /* installIfNotAvailable */
+                "12" /* minimumVersion */)
+        }
+
+        //INPUT E-MAIL DA METTERE O IMPOSTATO GIA AUTHUI
+
+        /*Firebase.auth.sendSignInLinkToEmail(email, actionCodeSettings)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Email sent.")
+                }
+            }*/
+
     }
 }
