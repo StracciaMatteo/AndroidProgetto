2c5fb83c9f648927321bc60a99894e25aca35786
Matteo Straccia <matteo.straccia98@gmail.com>
2023-01-31 16:14:12 +0100

    Registrazione pt.2

--- app/src/main/java/com/example/androidprogetto/registrazione.kt
+++ app/src/main/java/com/example/androidprogetto/registrazione.kt
@@ -1,5 +1,6 @@
 package com.example.androidprogetto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.firebase.ui.auth.AuthUI
@@ -8,34 +9,13 @@ import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
 
+
 class registrazione : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrazione)
 
-
-        // See: https://developer.android.com/training/basics/intents/result
-        private val signInLauncher = registerForActivityResult(
-            FirebaseAuthUIActivityResultContract()
-        ) { res ->
-            this.onSignInResult(res)
-        }
-
-        // Choose authentication providers
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build())
-
-
-
-// Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-        signInLauncher.launch(signInIntent)
-
-        private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        /*private*/ fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
             val response = result.idpResponse
             if (result.resultCode == RESULT_OK) {
                 // Successfully signed in
@@ -50,7 +30,26 @@ class registrazione : AppCompatActivity() {
             }
         }
 
+        
+        // See: https://developer.android.com/training/basics/intents/result
+/*private*/ val signInLauncher = registerForActivityResult(
+            FirebaseAuthUIActivityResultContract())
+            { res -> this.onSignInResult(res) }
 
+        // Choose authentication providers
+        val provider = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+
+
+// Create and launch sign-in intent
+        val signInInten = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(provider)
+            .build()
+        signInLauncher.launch(signInInten)
+        
 
         val actionCodeSettings = ActionCodeSettings.newBuilder()
             .setAndroidPackageName( /* yourPackageName= */
