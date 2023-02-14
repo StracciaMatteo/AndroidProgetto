b049ab04ce349f1f9aa27a5ea20e2db31d25f20d
Matteo Straccia <matteo.straccia98@gmail.com>
2023-02-14 12:37:04 +0100

    Aggiunto codice per iniettare i dati nella sezione profilo Utente

--- app/src/main/java/com/example/androidprogetto/visualizzaprofilo.kt
+++ app/src/main/java/com/example/androidprogetto/visualizzaprofilo.kt
@@ -2,10 +2,43 @@ package com.example.androidprogetto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class visualizzaprofilo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visualizza_profilo)
+
+        val user = Firebase.auth.currentUser
+        lateinit var name: String
+        lateinit var email: String
+        user?.let {
+            for (profile in it.providerData) {
+                // Id of the provider (ex: google.com)
+                val providerId = profile.providerId
+
+                // UID specific to the provider
+                val uid = profile.uid
+
+                // Name, email address, and profile photo Url
+                name = profile.displayName.toString()
+                email = profile.email.toString()
+                //val photoUrl = profile.photoUrl
+
+            }
+        }
+        val textName = findViewById<TextView>(R.id.textName)
+        textName.setText(name)
+
+        val textMail = findViewById<TextView>(R.id.textMail)
+        textMail.setText(email)
+
+        /*val textName = findViewById<TextView>(R.id.textName)
+        textName.setText(name)
+        val textMail = findViewById<TextView>(R.id.textMail)
+        textMail.setText(email)*/
+
     }
 }
