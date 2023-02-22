90e3556e9593ebfb5155a21ad32fc0bd3ebc0d88
Matteo Straccia <matteo.straccia98@gmail.com>
2023-02-22 11:42:31 +0100

    Creata la funzione SalvaRecensione che le carica sul DB

--- app/src/main/java/com/example/androidprogetto/caricamentorecensioni.kt
+++ app/src/main/java/com/example/androidprogetto/caricamentorecensioni.kt
@@ -1,11 +1,55 @@
 package com.example.androidprogetto
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class caricamentorecensioni : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_caricamento_recensioni)
+        val db = Firebase.firestore
+
+        fun salvaRecensione() {
+            val titolo = findViewById<EditText>(R.id.inputTitolo).getText().toString()
+
+            val testo = findViewById<EditText>(R.id.inputTestoRec).getText().toString()
+
+            val valutazione = findViewById<RatingBar>(R.id.ratingBarRec).getRating().toInt()
+
+            val recensioniKot = hashMapOf(
+                "Titolo" to titolo,
+                "Testo" to testo,
+                "Valutazione" to valutazione
+            )
+
+            // Add a new document with a generated ID
+            db.collection("recensioni2")
+                .add(recensioniKot)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(
+                        ContentValues.TAG,
+                        "DocumentSnapshot added with ID: ${documentReference.id}"
+                    )
+                }
+                .addOnFailureListener { e ->
+                    Log.w(ContentValues.TAG, "Error adding document", e)
+                }
+        }
+
+        /*FUNZIONE PER PASSARE AD UN'ALTRA ACTIVITY TRAMITE CLICK SUL BOTTONE E SALVARE LA RECENSIONE SUL DB*/
+        val buttonClick = findViewById<Button>(R.id.buttonSalvaRec)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            salvaRecensione()
+        }
     }
 }
--- app/src/main/java/com/example/androidprogetto/recensioni.kt
+++ app/src/main/java/com/example/androidprogetto/recensioni.kt
@@ -5,13 +5,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class recensioni : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recensioni)
+        val db = Firebase.firestore
 
 
         /*FUNZIONE PER PASSARE AD UN'ALTRA ACTIVITY TRAMITE CLICK SUL BOTTONE*/
@@ -21,5 +26,6 @@ class recensioni : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+        }
     }
-}
